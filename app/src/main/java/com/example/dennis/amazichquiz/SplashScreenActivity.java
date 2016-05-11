f5df2ea26a46fc6d9307b541d0cceb61ac58f77d
Xander <xanderdijk@hotmail.com>
2016-05-11 13:23:03 +0200

    Splash screen with 6 second delay

--- app/src/main/java/com/example/dennis/amazichquiz/SplashScreenActivity.java
+++ app/src/main/java/com/example/dennis/amazichquiz/SplashScreenActivity.java
@@ -1,5 +1,7 @@
 package com.example.dennis.amazichquiz;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +13,14 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-    }
-}
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, MenuActvity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 6000);
+
+    }}
